6bf60db3be92500e23d76ec10c9cffc1011776a4
VuHoangDiep <91532330+VuHoangDiep@users.noreply.github.com>
2022-10-15 12:58:40 +0700

    Add files via upload

---
+++ db_QLHS.sql
@@ -0,0 +1,184 @@
+-- db_qlhs
+-- tb_student
+	-- ID nvarchar
+    -- Name nvarchar
+    -- Mark Double
+    -- Khoi int
+    -- HanhKiem nvarchar
+    
+    
+-- Khoi 12
+-- 12A
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS01','Nguyễn Thị Ánh','12A',6.4,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS02','Nguyễn Văn Bình','12A',7.6,12,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS03','Nguyễn Vân Anh','12A',7.5,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS04','Nguyễn Hoàng Minh','12A',3.4,12,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS05','Trần Văn Sơn','12A',5.1,12,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS06','Hoàng Thị Mai','12A',5.0,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS07','Vũ Đình Ba','12A',6.2,12,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS08','Nguyễn Nhật Ánh','12A',8.9,12,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS09','Trần Công Minh','12A',9.2,12,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS10','Nguyễn Bình Anh','12A',6.5,12,'Khá');
+-- 12B
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS11','Đỗ Văn Vương','12B',6.0,12,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS12','Trần Diệu Linh','12B',7.4,12,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS13','Nguyễn Thị Hoa','12B',8.4,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS14','Mai Văn Việt','12B',8.6,12,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS15','Mai Thị Hoa','12B',8.4,12,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS16','Nguyễn Văn Công','12B',4.9,12,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS17','Đinh Văn Hoàng','12B',6.1,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS18','Đỗ Công Danh','12B',6.3,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS19','Mai Thị Huệ','12B',6.2,12,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS20','Trần Thị Xuân','12B',5.4,12,'Trung Bình');
+-- 12C
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS21','Nguyễn Văn Trường','12C',7.2,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS22','Đỗ Nhật Quang','12C',5.1,12,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS23','Nguyễn Văn Tình','12C',5.0,12,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS24','Vũ Thị Nhung','12C',10,12,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS25','Vũ Thị Hồng','12C',9.5,12,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS26','Vũ Văn Thiện','12C',4.1,12,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS27','Mai Văn Tài','12C',6.4,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS28','Lê Anh Bốn','12C',5.1,12,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS29','Mai Xuân Tuân','12C',7.3,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS30','Nguyễn Bá Cấp','12C',6.9,12,'Tốt');
+
+-- 12D
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS31','Phạm Thị Ngọc','12D',5.4,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS32','Nguyễn Thị Thắm','12D',6,12,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS33','Trần Ngọc Thắng','12D',7,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS34','Nguyễn Văn Thanh','12D',5,12,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS35','Nguyễn Thị Ánh Tuyết','12D',3.1,12,'Yếu');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS36','Trần Văn Ba','12D',8.1,12,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS37','Đào Văn Vũ','12D',6.7,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS38','Nguyễn Minh Trung','12D',6.9,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS39','Đào Thị Lệ','12D',5.4,12,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS40','Ngô Hồng Mai','12D',6,12,'Khá');
+
+-- 12E
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS41','Hoàng Hồng Mai','12E',5.3,12,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS42','Hoàng Thị Lệ','12E',6.4,12,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS43','Hoàng Minh Trung','12E',6.4,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS44','Nguyễn Văn Vũ','12E',7.9,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS45','Trần Thị Ánh Tuyết','12E',6.8,12,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS46','Nguyễn Văn Tài','12E',6.5,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS47','Nguyễn Minh Đức','12E',3.4,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS48','Nguyễn Ngọc Thắng','12E',5.6,12,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS49','Ngô Xuân Tuân','12E',8.9,12,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS50','Nguyễn Thị Huệ','12E',6.4,12,'Khá');
+
+-- Khoi 11
+-- 11A
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS51','Lê Xuân Tuân','11A',6.4,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS52','Lưu Thị Huệ','11A',6.4,11,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS53','Lê Minh Đức','11A',6.4,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS54','Đỗ Thị Ánh Tuyết','11A',6.4,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS55','Hoàng Minh Trung','11A',6.4,11,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS56','Hứa Văn Vũ','11A',6.4,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS57','Ngô Thị Hoa','11A',6.4,11,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS58','Hòng Văn Bình','11A',6.4,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS59','Lưu Thị Mai','11A',6.4,11,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS60','Lê Văn Vương','11A',6.4,11,'Khá');
+-- 11B
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS61','Lê Văn Thắng','11B',6.4,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS62','Ngô Thanh Tùng','11B',6.4,11,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS63','Hứa Hồng Mai','11B',6.4,11,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS64','Trần Thị Lệ','11B',6.4,11,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS65','Hoàng Bình Anh','11B',6.4,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS66','Lưu Văn Sơn','11B',6.4,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS67','Nguyễn Đình Ba','11B',6.4,11,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS68','Trần Thị Ánh','11B',6.4,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS69','Nguyễn Văn Thiện','11B',6.4,11,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS70','Nguyễn Công Danh','11B',6.4,11,'Tốt');
+-- 11C
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS71','Đào Công Danh','11C',6.4,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS72','Hứa Thị Hoa','11C',6.4,11,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS73','Nguyễn Diệu Linh','11C',6.4,11,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS74','Ngô Văn Sơn','11C',6.4,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS75','Hoàng Bình Anh','11C',6.4,11,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS76','Lê Đình Ba','11C',6.4,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS77','Nguyễn Hoàng Minh','11C',6.4,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS78','Lê Văn Bình','11C',6.4,11,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS79','Hồ Thị Mai','11C',6.4,11,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS80','Đỗ Vân Anh','11C',6.4,11,'Khá');
+-- 11D
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS81','Hồ Công Danh','11D',6.4,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS82','Lê Văn Bình','11D',7.6,11,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS83','Vũ Vân Anh','11D',7.5,11,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS84','Đào Hoàng Minh','11D',3.4,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS85','Hoàng Văn Sơn','11D',5.1,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS86','Lê Thị Mai','11D',5.0,11,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS87','Trần Đình Ba','11D',6.2,11,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS88','Cao Nhật Ánh','11D',8.9,11,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS89','Cao Công Minh','11D',9.2,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS90','Cao Bình Anh','11D',6.5,11,'Khá');
+-- 11E
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS91','Nguyễn Văn Vương','11E',6.0,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS92','Hứa Diệu Linh','11E',7.4,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS93','Lê Thị Hoa','11E',8.4,11,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS94','Trần Văn Việt','11E',8.6,11,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS95','Nguyễn Thị Hoa','11E',8.4,11,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS96','Mai Văn Công','11E',4.9,11,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS97','Nguyễn Văn Hoàng','11E',6.1,11,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS98','Lê Công Danh','11E',6.3,11,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS99','Cao Thị Huệ','11E',6.2,11,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS100','Nguyễn Thị Xuân','11E',5.4,11,'Khá');
+
+-- Khoi 10
+-- 10A
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS101','Nguyễn Văn Trường','10A',7.2,10,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS102','Đỗ Nhật Quang','10A',5.1,10,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS103','Nguyễn Văn Tình','10A',5.0,10,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS104','Vũ Thị Nhung','10A',10,10,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS105','Vũ Thị Hồng','10A',9.5,10,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS106','Vũ Văn Thiện','10A',4.1,10,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS107','Mai Văn Tài','10A',6.4,10,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS108','Lê Anh Bốn','10A',5.1,10,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS109','Mai Xuân Tuân','10A',7.3,10,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS110','Nguyễn Bá Cấp','10A',6.9,10,'Khá');
+
+-- 10B
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS111','Trần Thị Ngọc','10B',5.4,10,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS112','Trần Thị Thắm','10B',6,10,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS113','Nguyễn Ngọc Thắng','10B',7,10,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS114','Trần Văn Thanh','10B',5,10,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS115','Cao Thị Ánh Tuyết','10B',3.1,10,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS116','Lê Văn Ba','10B',8.1,10,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS117','Ngô Văn Vũ','10B',6.7,10,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS118','Đào Minh Trung','10B',6.9,10,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS119','Nguyễn Thị Lệ','10B',5.4,10,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS120','Nguyễn Hồng Mai','10B',6,10,'Khá');
+-- 10C
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS121','Ngô Thị Ánh','10C',6.4,10,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS122','Trần Đức Bình','10C',7.6,10,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS123','Đỗ Vân Anh','10C',7.5,10,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS124','Nguyễn Cao Minh','10C',3.4,10,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS125','Trần Đức Sơn','10C',5.1,10,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS126','Hoàng Như Mai','10C',5.0,10,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS127','Vũ Văn Ba','10C',6.2,10,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS128','Nguyễn Trần Ánh','10C',8.9,10,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS129','Trần Văn Minh','10C',9.2,10,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS130','Nguyễn Đõ Anh','10C',6.5,10,'Khá');
+-- 10D
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS131','Đỗ Quốc Vương','10D',6.0,10,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS132','Trần Thị Linh','10D',7.4,10,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS133','Nguyễn Như Hoa','10D',8.4,10,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS134','Mai Hoàng Việt','10D',8.6,10,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS135','Mai Như Hoa','10D',8.4,10,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS136','Nguyễn Thành Công','10D',4.9,10,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS137','Đinh Công Hoàng','10D',6.1,10,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS138','Đỗ Văn Doanh','10D',6.3,10,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS139','Lê Thị Huệ','10D',6.2,10,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS140','Nguyễn Như Xuân','10D',5.4,10,'Khá');
+-- 10E
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS141','Nguyễn Văn Trường','10E',7.2,10,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS142','Đỗ Nhật Quang','10E',5.1,10,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS143','Nguyễn Văn Tình','10E',5.0,10,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS144','Vũ Thị Nhung','10E',10,10,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS145','Vũ Thị Hồng','10E',9.5,10,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS146','Vũ Văn Thiện','10E',4.1,10,'Tốt');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS147','Mai Văn Tài','10E',6.4,10,'Khá');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS148','Lê Anh Bốn','10E',5.1,10,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS149','Mai Xuân Tuân','10E',7.3,10,'Trung Bình');
+insert into tb_student(ID,Name,ClassID,Mark,Khoi,HanhKiem) values('HS150','Nguyễn Bá Cấp','10E',6.9,10,'Khá');
+
+select * from tb_student;
